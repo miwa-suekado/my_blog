94fde1f78659bf50f1df73a74f6c9365e60060cd
miwa-suekado <bremen-undercover@ezweb.ne.jp>
2019-05-03 11:57:53 +0900

    リファクタリング完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,16 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
+
+
   def index
-    @posts = Post.all
-    @new_posts = Post.all
+    @posts = Post.order(created_at: :desc)
+    @new_posts = Post.order(created_at: :desc).limit(5)
     @author = Author.first
   end
 
   def show
-    @post = Post.find(params[:id])
+    
   end
 
   def new
@@ -21,25 +24,29 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    
   end
 
   def update
-    @post = Post.find(params[:id])
+    
     @post.update(post_params)
     redirect_to @post
   end
 
   def destroy
-    @post = Post.find(params[:id])
+    
     @post.destroy
     redirect_to  posts_path
   end
 
-  private
+  private   
 
   def post_params
     params.require(:post).permit(:title, :body, :category)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
